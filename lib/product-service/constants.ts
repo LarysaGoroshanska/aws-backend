03ca40af2cfca4c5e433a9a64e21948ed25cb719
Larysa_Goroshanska <Larysa_Goroshanska@epam.com>
2024-10-13 23:30:24 +0300

    finish task 3

---
+++ lib/product-service/constants.ts
@@ -0,0 +1,67 @@
+import { Product } from './types';
+
+export const products: Product[] = [
+  {
+    id: 'product-0',
+    title: 'Rainbow',
+    description: 'Very beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/2956.jpg',
+    price: 100,
+  },
+  {
+    id: 'product-1',
+    title: 'Do Not Miss Your Dream!',
+    description: 'Fantastic bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/1466.jpg',
+    price: 150,
+  },
+  {
+    id: 'product-2',
+    title: 'Sorceress',
+    description: 'Incredibly beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3911.jpg',
+    price: 120,
+  },
+  {
+    id: 'product-3',
+    title: 'Automn Melodie',
+    description: 'Very beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3527.jpg',
+    price: 200,
+  },
+  {
+    id: 'product-4',
+    title: 'Fairy Tale Forever',
+    description: 'Fantastic bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3510.jpg',
+    price: 160,
+  },
+  {
+    id: 'product-5',
+    title: 'First Love',
+    description: 'Incredibly beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3735.jpg',
+    price: 80,
+  },
+  {
+    id: 'product-6',
+    title: 'Catheline',
+    description: 'Very beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3920.jpg',
+    price: 135,
+  },
+  {
+    id: 'product-7',
+    title: 'Sympathy',
+    description: 'Fantastic bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/3182.jpg',
+    price: 79,
+  },
+  {
+    id: 'product-8',
+    title: 'Fairy Tale for Two',
+    description: 'Incredibly beautiful bouquet. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+    src: 'https://flowers.ua/images/Flowers/thumbnail/2060.jpg',
+    price: 220,
+  },
+];
