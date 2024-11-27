4d5f577e92d846c42b851f86e118f85d70ceecf5
Larysa_Goroshanska <Larysa_Goroshanska@epam.com>
2024-11-27 23:01:16 +0200

    upd

--- bin/aws-backend.ts
+++ bin/aws-backend.ts
@@ -8,6 +8,7 @@ import { HelloS3Stack } from '../lib/hello-s3/hello-s3-stack';
 import { ProductSqsStack } from '../lib/product-sqs/product-sqs-stack';
 import { ProductSnsStack } from '../lib/product-sns/product-sns-stack';
 import { AuthorizerStack } from '../lib/authorizer/authorizer-stack';
+import { HelloRdsStack } from '../lib/hello-rds/hello-rds-stack';
 
 const app = new cdk.App();
 new AwsBackendStack(app, 'AwsBackendStack', {
@@ -31,3 +32,10 @@ new HelloS3Stack(app, 'HelloS3Stack', {});
 new ProductSqsStack(app, 'ProductSqsStack');
 new ProductSnsStack(app, 'ProductSnsStack');
 new AuthorizerStack(app, 'AuthorizerStack');
+
+const envAPS  = { 
+  account: app.account, 
+  region: app.region, 
+};
+
+new HelloRdsStack(app, 'HelloRdsStack', { env: envAPS });
